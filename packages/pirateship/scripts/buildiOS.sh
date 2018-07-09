de00db558cde65905135aee951af73b4b725c8c2
Brett Weissbart <brett@brandingbrand.com>
2018-07-09 15:35:36 -0400

    fix add-keys scripts

--- packages/pirateship/scripts/buildiOS.sh
+++ packages/pirateship/scripts/buildiOS.sh
@@ -12,11 +12,11 @@ if [[ ${PIRATESHIP_CHANGED} == "0" ]]; then
     security default-keychain -s $KEY_CHAIN
     security unlock-keychain -p travis $KEY_CHAIN
     security set-keychain-settings -t 3600 -u $KEY_CHAIN
-    yarn ship:add-keys-ios
+    yarn add-keys-ios
     security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
 
     # script
-    cd ./packages/pirateship/ios/
+    cd ./ios/
     if test "$TRAVIS_PULL_REQUEST" = "false" ; then fastlane beta ; else fastlane build ; fi
 else
     echo 'build skipped because packages/pirateship was not modified'
