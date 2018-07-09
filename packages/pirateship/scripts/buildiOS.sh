637c60aba2f67002d165e1525cee412a77f4060b
Brett Weissbart <brett@brandingbrand.com>
2018-07-09 13:45:36 -0400

    try yarn instead of yarn prepare

--- packages/pirateship/scripts/buildiOS.sh
+++ packages/pirateship/scripts/buildiOS.sh
@@ -7,15 +7,6 @@ if [[ ${PIRATESHIP_CHANGED} == "0" ]]; then
     set -e
     echo 'packages/pirateship has been updated, rebuilding binary'
 
-    # get npm ready and node modules installed so we can init
-    gem uninstall cocoapods -x
-    gem install cocoapods -v 1.3.1
-    pod --version
-    pod repo update
-
-    yarn prepare
-    yarn ship:init ios
-
     mkdir -p $HOME/Library/MobileDevice/Provisioning\ Profiles/
     security create-keychain -p travis $KEY_CHAIN
     security default-keychain -s $KEY_CHAIN
